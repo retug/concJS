9b7ac94eb8c56778ee56d30994ae9d64cf1a9e53
Austin Guter <austinguter@gmail.com>
2025-01-27 20:54:31 -0800

    Hide Bar adjustments

--- src/materialsandShapes.js
+++ src/materialsandShapes.js
@@ -14,13 +14,20 @@ export function toggleMaterialsAndShapesDiv() {
       button.innerHTML = '&#xab;'; // Left-pointing double arrow
     } else {
       // Hide content and shrink the container
-      materialContent.style.display = 'none';
-      materialsAndShapes.style.width = `${button.offsetWidth}px`; // Set width to match button
+      button.style.position = 'absolute'; // Set button position to absolute
+      button.style.top = '10px'; // Set distance from the top of the screen
+      button.style.right = '20px'; // Set distance from the right edge of the screen
       button.innerHTML = '&#xbb;'; // Right-pointing double arrow
+      materialContent.style.display = 'none';
+      materialsAndShapes.style.width = `0`; // Set width to match button
+      materialsAndShapes.style.overflow = 'hidden'; // Disable scrolling
+      
+      button.innerHTML = '&#xbb;'; // Right-pointing double arrow
+      // Position the button in the upper-right corner
     }
   }
 
-  export function toggleShapeButtons() {
+export function toggleShapeButtons() {
     const rectangleButton = document.getElementById("rectangleButton");
     const barbellButton = document.getElementById("barbellButton");
   
@@ -33,5 +40,5 @@ export function toggleMaterialsAndShapesDiv() {
       barbellButton.classList.add("active");
       rectangleButton.classList.remove("active");
     });
-  }
+}
 
